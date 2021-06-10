37fc4f6a340769816bc639cd5ef8cd76ff9dbffa
Krystian Piękoś <krystian.piekos@infotraining.pl>
2021-06-10 16:17:46 +0200

    CTAD - 2

--- ctad/tests.cpp
+++ ctad/tests.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <numeric>
+#include <optional>
 #include <string>
 #include <vector>
 
@@ -87,7 +88,6 @@ namespace Explain
     }
 }
 
-
 TEST_CASE("deduce size of an array")
 {
     int tab1[34];
@@ -102,6 +102,27 @@ void deduce3(T&& arg)
     puts(__PRETTY_FUNCTION__);
 }
 
+// ///////////////
+// int x = 10;
+// arg is lvalue deduce3(x);
+
+// template <typename T = int&>
+// void deduce3(int& && -> int& arg)
+// {
+//     auto target = arg;
+//     puts(__PRETTY_FUNCTION__);
+// }
+
+///////////////
+// arg is rvalue - deduce3(get_number());
+
+// template <typename T = int>
+// void deduce3(int&& arg)
+// {
+//     auto target = std::move(arg);
+//     puts(__PRETTY_FUNCTION__);
+// }
+
 std::string get_name()
 {
     return "Jan"s;
@@ -110,18 +131,18 @@ std::string get_name()
 TEST_CASE("Template Argument Deduction - case 3")
 {
     std::string x = "text";
-    
+
     auto&& ax1 = x; // lvalue => auto&& -> string&
     static_assert(std::is_lvalue_reference_v<decltype(ax1)>);
-    
-    deduce3(x); // 
 
-    auto&& ax2 = get_name(); // rvalue => auto&& -> string&& 
+    deduce3(x); //
+
+    auto&& ax2 = get_name(); // rvalue => auto&& -> string&&
     static_assert(std::is_rvalue_reference_v<decltype(ax2)>);
 
     ax2.append("!!!");
 
-    deduce3("text"s);  
+    deduce3("text"s);
 
     deduce3(ax2);
     deduce3(std::move(ax2));
@@ -129,6 +150,31 @@ TEST_CASE("Template Argument Deduction - case 3")
     int tab[10];
 
     auto&& ax3 = tab; // int(&)[10]
+
+    std::cout << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n";
+
+    const std::string strx = "stringx";
+    const std::string& refstrx = strx;
+    std::string&& refrefstrx = "refrefstringx";
+    std::string target = std::move(refrefstrx);
+
+    deduce1(strx);
+    deduce1(refstrx);
+    deduce1(refrefstrx);
+
+    deduce2(strx);
+    deduce2(refstrx);
+    deduce2(refrefstrx);
+
+    deduce3(strx);
+    deduce3(refstrx);
+    deduce3(refrefstrx);
+
+    deduce3(std::move(strx));
+    deduce3(std::move(refstrx));
+    deduce3(std::move(refrefstrx));
+
+    std::cout << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n";
 }
 
 namespace Explain
@@ -167,4 +213,87 @@ TEST_CASE("vector explained")
     vec.push_back(text); // insert by copy
     vec.push_back("text"s); // insert by move
     vec.emplace_back(10, 'c'); // inserted in-place using args
+}
+
+template <typename T1, typename T2>
+struct ValuePair
+{
+    T1 fst;
+    T2 snd;
+
+    template <typename TFirst, typename TSecond>
+    ValuePair(TFirst&& f, TSecond&& s)
+        : fst {std::forward<TFirst>(f)}
+        , snd {std::forward<TSecond>(s)}
+    {
+    }
+};
+
+// deduction guides
+template <typename T1, typename T2>
+ValuePair(T1, T2) -> ValuePair<T1, T2>;
+
+ValuePair(const char*, const char*)->ValuePair<std::string, std::string>;
+
+TEST_CASE("CTAD")
+{
+    ValuePair<int, double> vp1 {1, 3.14};
+    ValuePair<int, std::string> vp2 {1, "text"};
+
+    ValuePair vp3 {1, 3.14};
+    ValuePair vp4(4, "text"); // ValuePair<int, const char*>
+    ValuePair vp5 {3.14, "text"s}; // ValuePair<double, string>
+    ValuePair vp6("abc", "defgh"); // ValuePair<std::string, std::string>
+}
+
+///////////////////////////
+// Aggregates + CTAD
+
+template <typename T>
+struct Aggregate1
+{
+    T value;
+};
+
+template <typename T>
+Aggregate1(T) -> Aggregate1<T>;
+
+TEST_CASE("CTAD - aggregates")
+{
+    Aggregate1 agg1 {1}; // Aggregate1<int>
+    Aggregate1 agg2 {"text"}; // Aggregate1<const char*>
+}
+
+TEST_CASE("CTAD + std library")
+{
+    int tab[10];
+    const int x = 10;
+
+    std::pair p1 {x, tab}; // pair<int, int*>
+    auto p2 = std::pair(1, 3.14);
+
+    std::tuple t1 {1, 3.14, "text", "text"s}; // std::tuple<int, double, const char*, string>
+    std::tuple t2 = t1; // std::tuple<int, double, const char*, string>
+    std::tuple t3 {t1}; // std::tuple<int, double, const char*, string>
+    std::tuple t4 = p2; // std::tuple<int, double>
+
+    std::optional opt1 = 42; // std::optional<int>
+    std::optional opt2 {3.14}; // std::optional<double>
+    std::optional opt3 {opt2}; // std::optional<double>
+
+    std::function f1 = foo; // function<void()>
+    std::function f2 = [](int x)
+    { std::cout << x << "\n"; }; // function<void(int)>
+
+    int* ptr = new int {5};
+    //std::unique_ptr uptr{ptr}; // ERROR - ill-formed (due to array type clash)
+    //std::shared_ptr sptr{ptr}; // ERROR - ill-formed (due to array type clash)
+
+    std::unique_ptr<int> uptr{ptr};
+    std::shared_ptr sptr = std::move(uptr); // shared_ptr<int>
+    std::weak_ptr wptr = sptr; // weak_ptr<int>
+
+    std::vector vec = {1, 2, 3, 4}; // std::vector<int>
+    std::vector vec2{vec}; // std::vector<int>
+    std::vector vec3{vec, vec}; // std::vector<std::vector<int>>
 }
